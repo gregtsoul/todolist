1297899473e167eaa2918a595f36227b039d3a9d
gregtsoul <106148263+gregtsoul@users.noreply.github.com>
2022-06-18 22:33:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import React from 'react';
 import './App.css';
 import Form from "./components/form/index";
 
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-
 
 function App() {
   return (
-    <Router>
       <Form />
-    </Router>
   );
 }
 
